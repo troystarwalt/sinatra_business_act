9817d085222dafaeeba2d2117ec3481594cd34e8
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-03 22:34:52 -0400

    Starting to add content

---
+++ app.rb
@@ -0,0 +1,50 @@
+require "sinatra"
+require "mandrill"
+require "geolocater"
+require "tilt/erubis"
+
+
+m = Mandrill::API.new
+
+set :bind, "0.0.0.0"
+
+get "/home" do
+	erb :index
+end
+
+get "/about" do
+	erb :about
+end
+
+get "/contact" do
+	erb :array
+end
+
+get "/products" do
+	erb :products
+end
+
+post "/sendmail" do
+	if !params[:u_email].nil? &&
+	   !params[:flname].nil? &&
+	   !params[:email_body].nil?
+
+		message = {
+				:subject=> "Hello from the sky!",
+				:from_name=> "Test",
+				:text=>"Word up?",
+				:to=>[{:email=> "#{params[:u_email]}",
+						:name=> "#{params[:fl_name]}"}
+					],
+				:html=>"<html>#{params[:email_body]}
+						<p><i>#{params[:flname]} sent you this message. You should thank them!
+						</p></i>
+						</html>",
+				:from_email=>"#{params[:f_email]}"
+			}
+		sending = m.messages.send(message)
+		"#{sending}"
+	else
+		"You forgot to fill out all of the fields!"
+	end
+end
