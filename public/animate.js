d49848f989fb8fbf9d72abf6b1e57dcf66e430fd
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-05 14:52:24 -0400

    Added Nav

--- public/animate.js
+++ public/animate.js
@@ -3,15 +3,23 @@
 
 // })
 
-// $("#heading")on("click", function(event){
-// 	.effect("bounce"), { times: 3}
+function clickMe() {
+	$("#heading_home").animate({
+		opacity: 0.50,
+	})
+}
+
+// $("div li")()
+
+// function clickMe(elmnt, clr) {
+// 	elmnt.style.color = clr;
 // }
 
-$("#button2").on("click", function(event){
-	$("#box")
-		.animate({width: "1000px"}, 7200)
-		.effect("bounce", "slow", { times:100, durantion:1})
-		.dequeue();
-	$("#boxhead")
-		.animate({fontSize: "50px"});
-})
+// $("#button2").on("click", function(event){
+// 	$("#box")
+// 		.animate({width: "1000px"}, 7200)
+// 		.effect("bounce", "slow", { times:100, durantion:1})
+// 		.dequeue();
+// 	$("#boxhead")
+// 		.animate({fontSize: "50px"});
+// })
