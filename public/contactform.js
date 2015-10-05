314a63a00c3772506e73979ebd5ee575217a5c86
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-05 17:38:58 -0400

    Adding wrapper to pages

---
+++ public/contactform.js
@@ -0,0 +1,9 @@
+ var str= name;
+ var capt = str.toUppercase;
+ var name = prompt("HELLO! WHATS YOUR NAME?");
+
+ var problem = prompt("HOW CAN WE HELP YOU TODAY " + name + + "?");
+
+ alert("OK " + name + ", " + "YOU CAN CHOOSE TO GET HELP THROUGH OUR LIVE CHAT OR SEND US AN E-MAIL(Replies within 24 hrs");
+
+ alert("YOU CAN ALSO CALL:1888-888-888 TO SPEAK TO A LIFE REP!");
