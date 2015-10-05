531728d8dac7463d1c34441ccb62fce45508057d
Christine Chan <Chan-Christine@hotmail.com>
2015-10-05 14:52:42 -0400

    Add ../images/energydrink.jpg

---
+++ images/squares.rb
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+.square {
+  background-color: #737373;
+  float: left;
+  position: relative;
+  width: 30%;
+  padding-bottom: 30.66%;
+  margin: 1.66%;
+}
+.square:hover {
+  background-color: black;
+}
+</head>
+
+<body>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+<div class="square"></div>
+</body>
+
+</html>
